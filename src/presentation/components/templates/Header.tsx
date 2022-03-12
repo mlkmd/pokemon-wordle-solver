56704a3e528b454efeceef6b18364ade6afc8adb
mlkmd <milkdromeda1021@gmail.com>
2022-03-12 13:55:02 +0900

    全体のスタイル変更

--- src/presentation/components/templates/Header.tsx
+++ src/presentation/components/templates/Header.tsx
@@ -2,6 +2,7 @@ import Style from 'presentation/components/templates/Header.module.scss';
 import HomeIcon from '@mui/icons-material/Home';
 import WidthManager from 'presentation/components/atoms/WidthManager';
 import { VFC } from 'react';
+import { Box } from '@mui/material';
 
 type Props = {
   /**
@@ -17,8 +18,10 @@ const Header: VFC<Props> = ({ subTitle, homeIconOnClick }) => {
   return (
     <header className={Style.header}>
       <WidthManager className={Style.titleContainer}>
-        <h1 className={Style.mainTitle}>Pokemon Wordle Solver</h1>
-        <HomeIcon className={Style.homeIcon} onClick={homeIconOnClick} />
+        <Box className={Style.mainTitleContainer}>
+          <h1 className={Style.mainTitle}>Pokemon Wordle Solver</h1>
+          <HomeIcon className={Style.homeIcon} onClick={homeIconOnClick} />
+        </Box>
         <h2 className={Style.subTitle}>{subTitle}</h2>
       </WidthManager>
     </header>
