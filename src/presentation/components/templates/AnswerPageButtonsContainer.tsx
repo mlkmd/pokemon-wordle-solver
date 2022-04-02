5fd99f282bbf899e4eb81cb1a97198919f205f94
mlkmd <milkdromeda1021@gmail.com>
2022-04-02 12:35:46 +0900

    modify layout

--- src/presentation/components/templates/AnswerPageButtonsContainer.tsx
+++ src/presentation/components/templates/AnswerPageButtonsContainer.tsx
@@ -1,34 +1,54 @@
 import { VFC } from 'react';
-import { Button, Grid } from '@mui/material';
+import { Box, Button, Grid } from '@mui/material';
 import GenerationSelector from 'presentation/components/atoms/GenerationSelector';
 import { AnswerPageData } from 'presentation/model/AnswerPageData';
+import Style from 'presentation/components/templates/AnswerPageRecommendContainer.module.scss';
 
 type Props = AnswerPageData;
 const AnswerPageButtonsContainer: VFC<Props> = ({
   generation,
+  selectedRecommend,
   onChangeGeneration,
+  onClickNextButton,
+  onSubmit,
   onClear,
+  onClearAll,
 }) => {
   return (
-    <Grid
-      container
-      justifyContent={'space-between'}
-      gap={4}
-      marginTop={'2rem'}
-      paddingX={'2rem'}
-    >
-      <Grid item>
-        <GenerationSelector
-          generation={generation}
-          onChange={onChangeGeneration}
-        />
-      </Grid>
-      <Grid item>
+    <Box className={Style.box}>
+      <GenerationSelector
+        generation={generation}
+        onChange={onChangeGeneration}
+      />
+      <Box className={Style.buttons}>
+        {typeof onClickNextButton !== 'undefined' && (
+          <Button
+            onClick={onClickNextButton}
+            variant={'contained'}
+            size={'large'}
+          >
+            次へ
+          </Button>
+        )}
+
+        <Button
+          disabled={selectedRecommend === null}
+          onClick={onSubmit}
+          variant={'contained'}
+          size={'large'}
+        >
+          決定
+        </Button>
         <Button onClick={onClear} variant={'contained'} size={'large'}>
           クリア
         </Button>
-      </Grid>
-    </Grid>
+        {typeof onClearAll !== 'undefined' && (
+          <Button onClick={onClearAll} variant={'contained'} size={'large'}>
+            最初から
+          </Button>
+        )}
+      </Box>
+    </Box>
   );
 };
 export default AnswerPageButtonsContainer;
